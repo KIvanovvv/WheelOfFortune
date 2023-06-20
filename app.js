d095cd91a780cc723b0b781587be1c558482076f
unknown <k.ivanov29@yahoo.com>
2023-06-20 16:03:07 +0300

    Adding header, credits, background image and sound effect

--- app.js
+++ app.js
@@ -34,14 +34,25 @@ const cell14Value = cell14.querySelector(`b`).textContent;
 const cell15Value = cell15.querySelector(`b`).textContent;
 const cell16Value = cell16.querySelector(`b`).textContent;
 
+const balanceElement = document.querySelector(`.balance`);
+const heading = document.querySelector(`.heading`);
+
+const wheelSpinSound = new Audio(`./assets/sound/wheelSound.wav`);
 let btnClicked = false;
 let initialDeg = 0;
 let spins = 0;
+let balance = 0;
+let totalWin = 0;
+let currentWin = 0;
+
 btnStart.addEventListener(`click`, () => {
   if (!btnClicked) {
     if (spins === 10) {
       spins = 0;
     }
+    wheelSpinSound.play();
+    heading.style.color = "white";
+    heading.textContent = `Желаем Ви успех!`;
     spins++;
     btnClicked = true;
     let spinDeg = 0;
@@ -125,36 +136,68 @@ btnStart.addEventListener(`click`, () => {
     }
 
     if (spinDeg <= 11.2 || spinDeg >= 348.8) {
+      currentWin = Number(cell3Value);
+      totalWin += Number(cell3Value);
       console.log(cell3Value);
     } else if (spinDeg <= 33.7 && spinDeg >= 11.3) {
+      currentWin = Number(cell10Value);
+      totalWin += Number(cell10Value);
       console.log(cell10Value);
     } else if (spinDeg <= 56.3 && spinDeg >= 33.8) {
+      currentWin = Number(cell9Value);
+      totalWin += Number(cell9Value);
       console.log(cell9Value);
     } else if (spinDeg <= 78.9 && spinDeg >= 56.4) {
+      currentWin = Number(cell14Value);
+      totalWin += Number(cell14Value);
       console.log(cell14Value);
     } else if (spinDeg <= 101.5 && spinDeg >= 79) {
+      currentWin = Number(cell1Value);
+      totalWin += Number(cell1Value);
       console.log(cell1Value);
     } else if (spinDeg <= 124.1 && spinDeg >= 101.6) {
+      currentWin = Number(cell11Value);
+      totalWin += Number(cell11Value);
       console.log(cell11Value);
     } else if (spinDeg <= 146.7 && spinDeg >= 124.2) {
+      currentWin = Number(cell7Value);
+      totalWin += Number(cell7Value);
       console.log(cell7Value);
     } else if (spinDeg <= 169.3 && spinDeg >= 146.8) {
+      currentWin = Number(cell15Value);
+      totalWin += Number(cell15Value);
       console.log(cell15Value);
     } else if (spinDeg <= 191.9 && spinDeg >= 169.4) {
+      currentWin = Number(cell4Value);
+      totalWin += Number(cell4Value);
       console.log(cell4Value);
     } else if (spinDeg <= 214.5 && spinDeg >= 192) {
+      currentWin = Number(cell6Value);
+      totalWin += Number(cell6Value);
       console.log(cell6Value);
     } else if (spinDeg <= 237.1 && spinDeg >= 214.6) {
+      currentWin = Number(cell5Value);
+      totalWin += Number(cell5Value);
       console.log(cell5Value);
-    } else if (spinDeg <= 259.7 && spinDeg >= 237.2) {
+    } else if (spinDeg <= 259 && spinDeg >= 237.2) {
+      currentWin = Number(cell13Value);
+      totalWin += Number(cell13Value);
       console.log(cell13Value);
-    } else if (spinDeg <= 282.3 && spinDeg >= 259.8) {
+    } else if (spinDeg <= 282.3 && spinDeg >= 259) {
+      currentWin = Number(cell2Value);
+      totalWin += Number(cell2Value);
       console.log(cell2Value);
     } else if (spinDeg <= 304.9 && spinDeg >= 282.4) {
+      currentWin = Number(cell12Value);
+      totalWin += Number(cell12Value);
       console.log(cell12Value);
     } else if (spinDeg <= 327.5 && spinDeg >= 305) {
+      currentWin = Number(cell8Value);
+      totalWin += Number(cell8Value);
       console.log(cell8Value);
     } else if (spinDeg <= 348.7 && spinDeg >= 327.6) {
+      currentWin = Number(cell16Value);
+      totalWin += Number(cell16Value);
       console.log(cell16Value);
     }
 
@@ -167,6 +210,9 @@ btnStart.addEventListener(`click`, () => {
       wheelElement.style.transition = `all 0s `;
       wheelElement.style.transform = `rotate(${spinDeg - 720}deg)`;
       btnClicked = false;
+      balanceElement.textContent = totalWin;
+      heading.style.color = "green";
+      heading.textContent = `Поздравления! Вие спечелихте ${currentWin} кредита!`;
     }, 5000);
   }
 });

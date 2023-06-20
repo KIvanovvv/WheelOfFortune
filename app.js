fdeb9309c6d0db8385d0a3de7df0ebc20d9c3f75
unknown <k.ivanov29@yahoo.com>
2023-06-20 19:19:36 +0300

    Adding sound and FreeSpins feature

--- app.js
+++ app.js
@@ -34,16 +34,21 @@ const cell14Value = cell14.querySelector(`b`).textContent;
 const cell15Value = cell15.querySelector(`b`).textContent;
 const cell16Value = cell16.querySelector(`b`).textContent;
 
-const balanceElement = document.querySelector(`.balance`);
+const balanceBoard = document.querySelector(`.balance`);
 const heading = document.querySelector(`.heading`);
+const backdrop = document.querySelector(`.backdrop`);
+const bonusGame = document.querySelector(`.bonus-game`);
 
 const wheelSpinSound = new Audio(`./assets/sound/wheelSound.wav`);
+const freeSpinSound = new Audio(`./assets/sound/freeSpinsSound.wav`);
 let btnClicked = false;
 let initialDeg = 0;
 let spins = 0;
 let balance = 0;
 let totalWin = 0;
 let currentWin = 0;
+let freeSpinsTriggered = false;
+let freeSpins = 0;
 
 btnStart.addEventListener(`click`, () => {
   if (!btnClicked) {
@@ -67,7 +72,7 @@ btnStart.addEventListener(`click`, () => {
       spinDeg = rngVar;
     }
     if (spins === 2) {
-      spinDeg = Math.random() * 22.5 + 79; //On Spin 2 always land on cell 1
+      spinDeg = Math.random() * 21 + 79; //On Spin 2 always land on cell 1
     }
     if (spins === 3) {
       //Spin 3 is random
@@ -83,7 +88,7 @@ btnStart.addEventListener(`click`, () => {
       spinDeg = rngVar;
     }
     if (spins === 4) {
-      spinDeg = Math.random() * 22.5 + 259.8; //On Spin 4 always land on cell 2
+      spinDeg = Math.random() * 21 + 259.8; //On Spin 4 always land on cell 2
     }
     if (spins === 5) {
       //Spin 5 is random
@@ -99,7 +104,7 @@ btnStart.addEventListener(`click`, () => {
       spinDeg = rngVar;
     }
     if (spins === 6) {
-      spinDeg = Math.random() * 22.5 + 79; //On Spin 6 always land on cell 1
+      spinDeg = Math.random() * 21 + 79; //On Spin 6 always land on cell 1
     }
     if (spins === 7) {
       //Spin 7 is random
@@ -115,7 +120,7 @@ btnStart.addEventListener(`click`, () => {
       spinDeg = rngVar;
     }
     if (spins === 8) {
-      spinDeg = Math.random() * 22.5 + 259.8; //On Spin 8 always land on cell 2
+      spinDeg = Math.random() * 21 + 259.8; //On Spin 8 always land on cell 2
     }
     if (spins === 9) {
       //Spin 9 is random
@@ -131,77 +136,12 @@ btnStart.addEventListener(`click`, () => {
       spinDeg = rngVar;
     }
     if (spins === 10) {
-      spinDeg = Math.random() * 22.5 + 79; //On Spin 10 always land on cell 1 and reset spins counter
+      spinDeg = Math.random() * 21 + 79; //On Spin 10 always land on cell 1 and reset spins counter
       spins = 0;
     }
+    spinDeg = 256; // Uncomment to check Free Spins
+    checkValue(spinDeg);
 
-    if (spinDeg <= 11.2 || spinDeg >= 348.8) {
-      currentWin = Number(cell3Value);
-      totalWin += Number(cell3Value);
-      console.log(cell3Value);
-    } else if (spinDeg <= 33.7 && spinDeg >= 11.3) {
-      currentWin = Number(cell10Value);
-      totalWin += Number(cell10Value);
-      console.log(cell10Value);
-    } else if (spinDeg <= 56.3 && spinDeg >= 33.8) {
-      currentWin = Number(cell9Value);
-      totalWin += Number(cell9Value);
-      console.log(cell9Value);
-    } else if (spinDeg <= 78.9 && spinDeg >= 56.4) {
-      currentWin = Number(cell14Value);
-      totalWin += Number(cell14Value);
-      console.log(cell14Value);
-    } else if (spinDeg <= 101.5 && spinDeg >= 79) {
-      currentWin = Number(cell1Value);
-      totalWin += Number(cell1Value);
-      console.log(cell1Value);
-    } else if (spinDeg <= 124.1 && spinDeg >= 101.6) {
-      currentWin = Number(cell11Value);
-      totalWin += Number(cell11Value);
-      console.log(cell11Value);
-    } else if (spinDeg <= 146.7 && spinDeg >= 124.2) {
-      currentWin = Number(cell7Value);
-      totalWin += Number(cell7Value);
-      console.log(cell7Value);
-    } else if (spinDeg <= 169.3 && spinDeg >= 146.8) {
-      currentWin = Number(cell15Value);
-      totalWin += Number(cell15Value);
-      console.log(cell15Value);
-    } else if (spinDeg <= 191.9 && spinDeg >= 169.4) {
-      currentWin = Number(cell4Value);
-      totalWin += Number(cell4Value);
-      console.log(cell4Value);
-    } else if (spinDeg <= 214.5 && spinDeg >= 192) {
-      currentWin = Number(cell6Value);
-      totalWin += Number(cell6Value);
-      console.log(cell6Value);
-    } else if (spinDeg <= 237.1 && spinDeg >= 214.6) {
-      currentWin = Number(cell5Value);
-      totalWin += Number(cell5Value);
-      console.log(cell5Value);
-    } else if (spinDeg <= 259 && spinDeg >= 237.2) {
-      currentWin = Number(cell13Value);
-      totalWin += Number(cell13Value);
-      console.log(cell13Value);
-    } else if (spinDeg <= 282.3 && spinDeg >= 259) {
-      currentWin = Number(cell2Value);
-      totalWin += Number(cell2Value);
-      console.log(cell2Value);
-    } else if (spinDeg <= 304.9 && spinDeg >= 282.4) {
-      currentWin = Number(cell12Value);
-      totalWin += Number(cell12Value);
-      console.log(cell12Value);
-    } else if (spinDeg <= 327.5 && spinDeg >= 305) {
-      currentWin = Number(cell8Value);
-      totalWin += Number(cell8Value);
-      console.log(cell8Value);
-    } else if (spinDeg <= 348.7 && spinDeg >= 327.6) {
-      currentWin = Number(cell16Value);
-      totalWin += Number(cell16Value);
-      console.log(cell16Value);
-    }
-
-    console.log(spinDeg);
     spinDeg = spinDeg + 720;
     wheelElement.style.transition = `all 5s ease `;
     wheelElement.style.transform = `rotate(${spinDeg}deg)`;
@@ -210,9 +150,141 @@ btnStart.addEventListener(`click`, () => {
       wheelElement.style.transition = `all 0s `;
       wheelElement.style.transform = `rotate(${spinDeg - 720}deg)`;
       btnClicked = false;
-      balanceElement.textContent = totalWin;
-      heading.style.color = "green";
-      heading.textContent = `Поздравления! Вие спечелихте ${currentWin} кредита!`;
+      balanceBoard.textContent = totalWin;
+
+      if (freeSpinsTriggered) {
+        backdrop.style.display = "block";
+        bonusGame.style.display = "flex";
+        freeSpinSound.play();
+        //Inside free spins
+        setTimeout(() => {
+          backdrop.style.display = "none";
+          bonusGame.style.display = "none";
+
+          heading.style.color = "purple";
+          heading.textContent = `Брой безплатни завъртания ${freeSpins} от 3`;
+          btnClicked = true;
+          bonusGameSpins();
+        }, 4000);
+        //End of free spins
+      } else {
+        heading.style.color = "green";
+        heading.textContent = `Поздравления! Вие спечелихте ${currentWin} кредита!`;
+      }
     }, 5000);
   }
 });
+
+function checkValue(spinDeg, isBonusTriggered) {
+  let bonusWin = 0;
+  if (spinDeg <= 11.2 || spinDeg >= 348.8) {
+    currentWin = Number(cell3Value);
+    totalWin += Number(cell3Value);
+    console.log(cell3Value);
+  } else if (spinDeg <= 33.7 && spinDeg >= 11.3) {
+    currentWin = Number(cell10Value);
+    totalWin += Number(cell10Value);
+    console.log(cell10Value);
+  } else if (spinDeg <= 56.3 && spinDeg >= 33.8) {
+    currentWin = Number(cell9Value);
+    totalWin += Number(cell9Value);
+    console.log(cell9Value);
+  } else if (spinDeg <= 78.9 && spinDeg >= 56.4) {
+    currentWin = Number(cell14Value);
+    totalWin += Number(cell14Value);
+    console.log(cell14Value);
+  } else if (spinDeg <= 101.5 && spinDeg >= 79) {
+    currentWin = Number(cell1Value);
+    totalWin += Number(cell1Value);
+    console.log(cell1Value);
+  } else if (spinDeg <= 124.1 && spinDeg >= 101.6) {
+    currentWin = Number(cell11Value);
+    totalWin += Number(cell11Value);
+    console.log(cell11Value);
+  } else if (spinDeg <= 146.7 && spinDeg >= 124.2) {
+    currentWin = Number(cell7Value);
+    totalWin += Number(cell7Value);
+    console.log(cell7Value);
+  } else if (spinDeg <= 169.3 && spinDeg >= 146.8) {
+    currentWin = Number(cell15Value);
+    totalWin += Number(cell15Value);
+    console.log(cell15Value);
+  } else if (spinDeg <= 191.9 && spinDeg >= 169.4) {
+    currentWin = Number(cell4Value);
+    totalWin += Number(cell4Value);
+    console.log(cell4Value);
+  } else if (spinDeg <= 214.5 && spinDeg >= 192) {
+    currentWin = Number(cell6Value);
+    totalWin += Number(cell6Value);
+    console.log(cell6Value);
+  } else if (spinDeg <= 237.1 && spinDeg >= 214.6) {
+    currentWin = Number(cell5Value);
+    totalWin += Number(cell5Value);
+    console.log(cell5Value);
+  } else if (spinDeg <= 259 && spinDeg >= 237.2) {
+    freeSpinsTriggered = true;
+    console.log(`You win Free Spins`);
+    console.log(cell13Value);
+  } else if (spinDeg <= 282.3 && spinDeg >= 259) {
+    currentWin = Number(cell2Value);
+    totalWin += Number(cell2Value);
+    console.log(cell2Value);
+  } else if (spinDeg <= 304.9 && spinDeg >= 282.4) {
+    currentWin = Number(cell12Value);
+    totalWin += Number(cell12Value);
+    console.log(cell12Value);
+  } else if (spinDeg <= 327.5 && spinDeg >= 305) {
+    currentWin = Number(cell8Value);
+    totalWin += Number(cell8Value);
+    console.log(cell8Value);
+  } else if (spinDeg <= 348.7 && spinDeg >= 327.6) {
+    currentWin = Number(cell16Value);
+    totalWin += Number(cell16Value);
+    console.log(cell16Value);
+  }
+  if (isBonusTriggered) {
+    bonusWin = currentWin;
+    return bonusWin;
+  }
+}
+
+function bonusSpin() {
+  heading.style.color = "purple";
+  freeSpins++;
+  heading.textContent = `Брой безплатни завъртания ${freeSpins} от 3`;
+  spinDeg = Math.random() * 360 + 720;
+  wheelElement.style.transition = `all 5s ease `;
+  wheelElement.style.transform = `rotate(${spinDeg}deg)`;
+  wheelSpinSound.play();
+  setTimeout(() => {
+    wheelElement.style.transition = `all 0s `;
+    wheelElement.style.transform = `rotate(${spinDeg - 720}deg)`;
+  }, 5000);
+  return spinDeg - 720;
+}
+function bonusGameSpins() {
+  let bonusWin = 0;
+  let spin1Deg = 0;
+  let spin2Deg = 0;
+  let spin3Deg = 0;
+  spin1Deg = bonusSpin();
+  setTimeout(() => {
+    spin2Deg = bonusSpin();
+  }, 6000);
+  setTimeout(() => {
+    spin3Deg = bonusSpin();
+  }, 12000);
+  setTimeout(() => {
+    console.log(spin1Deg, spin2Deg, spin3Deg);
+    let win1 = checkValue(spin1Deg, true);
+    let win2 = checkValue(spin2Deg, true);
+    let win3 = checkValue(spin3Deg, true);
+    bonusWin = win1 + win2 + win3;
+    balance += bonusWin;
+    balanceBoard.textContent = balance;
+    heading.style.color = "green";
+    heading.textContent = `Поздравления! Вие спечелихте ${bonusWin} кредита от бонус играта!`;
+    btnClicked = false;
+    freeSpins = 0;
+  }, 18000);
+}
